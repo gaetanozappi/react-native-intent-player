ee0575589b9505e504156f056429ea62ff488f85
gaetanozappi <gaetano.zappala.94@gmail.com>
2018-07-31 17:13:17 +0200

    Update SimpleVideoStream.java

--- android/app/src/main/java/com/reactlibrary/SimpleVideoStream.java
+++ android/app/src/main/java/com/reactlibrary/SimpleVideoStream.java
@@ -192,20 +192,22 @@ public class SimpleVideoStream extends Activity implements
         // The screen size changed or the orientation changed... don't restart the activity
         super.onConfigurationChanged(newConfig);
     }
-
+    
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (mMediaController != null){
-            mMediaController.show();
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
             this.getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-              | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-              | View.SYSTEM_UI_FLAG_IMMERSIVE
-              );
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            this.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
-        return false;
     }
 }
